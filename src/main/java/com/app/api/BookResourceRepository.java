eb3c84320d96a7a43222632b4c18298240aa9e60
Shreyansh Vyas <vyasshreyansh71@gmail.com>
2024-02-19 12:34:33 +0530

    :sparkles: Add crud repo

---
+++ src/main/java/com/app/api/BookResourceRepository.java
@@ -0,0 +1,8 @@
+package com.app.api;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "book", path = "books-resource")
+interface BookResourceRepository extends PagingAndSortingRepository<Book, Long>, CrudRepository<Book, Long>{}
